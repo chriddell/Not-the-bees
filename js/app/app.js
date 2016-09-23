9920c5823e508e7c702fab3261366f5e71a52b81
Chris Liddell <chrisliddell1991@gmail.com>
2016-09-23 14:46:57 +0100

    Seed in. Logo in. Animations work through to end of app.

--- js/app/app.js
+++ js/app/app.js
@@ -5,7 +5,7 @@
    ========================================================================== */
 
 var currentSection = 1,
-    totalSections = 10,
+    totalSections = 9,
     scrollAllowed = true,
     usefulClassName = 'step';
 
@@ -71,7 +71,9 @@ function showKeyPanel( clicked ) {
   // target has similar id to clicked so let's merge
   var targetID = '#large-' + self.id;
 
-  $('#honeycomb-key-panels').find(targetID)
+  $('#honeycomb-key-panels')
+    .addClass('honeycomb-key-panels--active')
+    .find(targetID)
     .css({
       'visibility': 'visible'
     });
@@ -85,9 +87,13 @@ function hideKeyPanel( clicked ) {
 
   var self = clicked;
 
-  $(self).parents('.honeycomb__key-panel--large').css({
-    'visibility': 'hidden'
-  });
+  $(self)
+    .parents('.honeycomb__key-panel--large')
+    .css({
+      'visibility': 'hidden'
+    })
+    .parents('#honeycomb-key-panels')
+    .removeClass('honeycomb-key-panels--active');
 
 };
 
@@ -108,68 +114,72 @@ $(document).ready(function(){
      * http://www.javascriptkit.com/javatutors/onmousewheel.shtml
      */
 
-      e.preventDefault();
-      e.stopPropagation();
+      if (!$('body').hasClass('no-scroll')) {
 
-      /**
-       * Set scrollAllowed back to true
-       * after a designated interval
-       */
-      clearTimeout( $.data( this, 'timer' ) );
-      $.data(this, 'timer', setTimeout(function() {
-        scrollAllowed = true;
-      }, 250));
+        e.preventDefault();
+        e.stopPropagation();
 
-      if ( scrollAllowed ) {
-
-        /*
-         * Set scrollAllowed to false 
-         * to prevent currentSection changing twice
+        /**
+         * Set scrollAllowed back to true
+         * after a designated interval
          */
-        scrollAllowed = false;
+        clearTimeout( $.data( this, 'timer' ) );
+        $.data(this, 'timer', setTimeout(function() {
+          scrollAllowed = true;
+        }, 250));
 
-        /*
-         * jQuery mousewheel direction capture
-         * works cross-browser
-         */
-        if ( typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0 ) {
+        if ( scrollAllowed ) {
 
-          if( e.originalEvent.detail > 0 && currentSection < totalSections ) {
+          /*
+           * Set scrollAllowed to false 
+           * to prevent currentSection changing twice
+           */
+          scrollAllowed = false;
 
-            // Next section
-            currentSection++;
+          /*
+           * jQuery mousewheel direction capture
+           * works cross-browser
+           */
+          if ( typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0 ) {
 
-          } else if( e.originalEvent.detail < 0 && currentSection > 1 ){
+            if( e.originalEvent.detail > 0 && currentSection < totalSections ) {
 
-            // Previous section
-            currentSection--;
+              // Next section
+              currentSection++;
 
+            } else if( e.originalEvent.detail < 0 && currentSection > 1 ){
+
+              // Previous section
+              currentSection--;
+
+            }
+
+          } else if ( typeof e.originalEvent.wheelDelta == 'number' ) {
+
+            if ( e.originalEvent.wheelDelta < 0 && currentSection < totalSections ) {
+
+              // Next section
+              currentSection++;
+
+            } else if ( e.originalEvent.wheelDelta > 0 && currentSection > 1 ) {
+
+              // Previous section
+              currentSection--;
+
+            }
           }
 
-        } else if ( typeof e.originalEvent.wheelDelta == 'number' ) {
+          $('#main-container').alterClass(usefulClassName + '-*', usefulClassName + '-' +  currentSection + '-active');
 
-          if ( e.originalEvent.wheelDelta < 0 && currentSection < totalSections ) {
+        } else {
 
-            // Next section
-            currentSection++;
+          // Do nothing...
+          return false;
 
-          } else if ( e.originalEvent.wheelDelta > 0 && currentSection > 1 ) {
-
-            // Previous section
-            currentSection--;
-
-          }
         }
-
-        $('#main-container').alterClass(usefulClassName + '-*', usefulClassName + '-' +  currentSection + '-active');
-
-      } else {
-
-        // Do nothing...
-        return false;
-
+        
       }
-      
+
     }
   });
 
@@ -177,8 +187,25 @@ $(document).ready(function(){
 
 $('.honeycomb__key-panel').on('click', function(){
   showKeyPanel(this);
+  $('body').addClass('no-scroll');
 });
 
 $('.honeycomb__key-panel--large__close').on('click', function(){
   hideKeyPanel(this);
-})
+  $('body').removeClass('no-scroll');
+});
+
+
+/* ==========================================================================
+   Development functions
+
+   Burn after using.
+   ========================================================================== */
+var startingLevel = 4;
+
+currentSection = startingLevel;
+
+$(document).ready(function(){
+
+  $('#main-container').addClass(usefulClassName + '-' +  currentSection + '-active');
+});
